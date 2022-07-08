28081815fbc4929356dc3d45f3a428fed7f72495
tetianaborysova <tetiana.borysova@empeek.io>
2022-07-08 15:49:48 +0200

    initial commit

--- Cryptocurrency Price Tracker/ViewController.swift
+++ Cryptocurrency Price Tracker/ViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var crpCcy: [String] = [];
+    var ccy: [String] = [];
 
+    @IBOutlet weak var price: UILabel!
+    
+    @IBOutlet weak var picker: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        crpCcy = ["BTC", "ETH", "XRP", "BCH"]
+        ccy = ["USD", "EUR", "JPY", "CHF"]
         // Do any additional setup after loading the view.
     }
 
